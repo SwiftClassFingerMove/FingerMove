a02f66e2505eabf910f66d4a2b39f9a6c4a53955
jeffhuang0217 <jeffhuang0217@gmail.com>
2015-05-11 16:57:24 +0800

    step02
    開啟多點觸控

--- FingerMove/ViewController.swift
+++ FingerMove/ViewController.swift
@@ -4,6 +4,10 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var toolBar: UIToolbar!
     
+    //記錄觸碰的起點與終點
+    var touchStartPosition:CGPoint!
+    var touchEndPosition:CGPoint!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -21,7 +25,32 @@ class ViewController: UIViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Touch Events
+    //觸碰開始
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+        super.touchesBegan(touches, withEvent: event)
+        //記錄的觸碰點歸零
+        touchStartPosition = CGPointMake(0, 0)
+        touchEndPosition = CGPointMake(0, 0)
+        
+        println("\(touches.count)")
+//        if let touch = touches.count
+        
+        println("觸碰開始")
+    }
+    //觸碰中移動
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+        super.touchesMoved(touches, withEvent: event)
+        println("觸碰中移動")
+    }
+    //觸碰結束
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+        super.touchesEnded(touches, withEvent: event)
+        println("觸碰結束")
+    }
 }
 
